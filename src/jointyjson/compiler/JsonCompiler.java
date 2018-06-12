39d45c8a7ca6ee9b1c490b8f4b68aad2f21c5419
Salvatore Giampà <sv.giampa@gmail.com>
2018-06-12 20:27:34 +0200

    Update of the JointyC Library

--- src/jointyjson/compiler/JsonCompiler.java
+++ src/jointyjson/compiler/JsonCompiler.java
@@ -71,7 +71,7 @@ public class JsonCompiler {
 		return null;
 	}
 	
-	/*
+	
 	public static void main(String[] args) throws IOException {
 		String jsonSource = new String(Files.readAllBytes(new File("test.json").toPath()));
 		JsonCompiler jsonc = new JsonCompiler();
@@ -86,5 +86,5 @@ public class JsonCompiler {
 		System.out.println("\nRe-encoded JSON:\n");
 		System.out.println(encoder.getJsonString());
 	}
-	*/
+	
 }
